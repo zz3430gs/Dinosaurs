a0b76d6b8ef525a98541a95ecc785a2e252c1dcf
zz3430gs <keigolis95@gmail.com>
2015-09-28 13:33:59 -0500

    Added some comments

--- lab5cont.java
+++ lab5cont.java
@@ -4,6 +4,7 @@ public class lab5cont {
 
     public static void main(String[] args) {
 
+        //Create variables with the amount of the coin
         int dollar = 100;
         int quarter = 25;
         int dime = 10;
@@ -11,6 +12,7 @@ public class lab5cont {
         int penny = 1;
         int combo = 0;
 
+        //Create loops, then when the loops adds up to 100 the variable combo will add 1 every time
         for (int p = 0; p <= 100; p++) {
             for (int n = 0; n <= 20; n++) {
                 for (int d = 0; d <= 10; d++) {
@@ -22,7 +24,8 @@ public class lab5cont {
                 }
             }
         }
-                    System.out.println("The total combination is " + combo);
+        //Print out how many different combinations there are
+        System.out.println("The total combination is " + combo);
             }
         }
 
